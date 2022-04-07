885d7542218e7322351a7b44796b122ff2584f5d
Ben Schlegel <brschlegels@gmail.com>
2022-04-06 20:36:41 -0400

    Info Scene + TMP

---
+++ PrototypingGame6/Assets/Scritps/BeginButton.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class BeginButton : MonoBehaviour
+{
+
+    public float delay;
+    public Transform button;
+    // Start is called before the first frame update
+    void Start()
+    {
+        StartCoroutine(Enable());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public IEnumerator Enable()
+    {
+        button.gameObject.SetActive(false);
+        yield return new WaitForSeconds(delay);
+        button.gameObject.SetActive(true);
+    }
+}
