f3006ba9d52e25c78e841036af3079b7c66b4ffc
chaulotj <chaulotj@gmail.com>
2022-04-29 18:39:56 -0400

    ctrl z

--- PrototypingGame6/Assets/Scripts/Undo.cs
+++ PrototypingGame6/Assets/Scripts/Undo.cs
@@ -8,23 +8,37 @@ public class Undo : DrawingButtons
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Mouse0) && Paint.actionStack.Count > 0)
+        bool hitCtrlZ = false;
+        if(Input.GetKey(KeyCode.LeftControl) && Input.GetKeyDown(KeyCode.Z))
+        {
+            hitCtrlZ = true;
+        }
+        if ((hitCtrlZ || Input.GetKeyDown(KeyCode.Mouse0)) && Paint.actionStack.Count > 0)
         {
             RaycastHit hit;
-            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
+            if (hitCtrlZ)
+            {
+                DoUndo();
+            }
+            else if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit))
             {
                 Undo undo = hit.collider.GetComponent<Undo>();
                 if (undo != null)
                 {
-                    buttonGraying.GrayButtons(type, id);
-                    List<(int, int, Color)> list = Paint.actionStack.Pop();
-                    foreach((int, int, Color) c in list)
-                    {
-                        Drawing.Texture.SetPixel(c.Item1, c.Item2, c.Item3);
-                    }
-                    Drawing.Texture.Apply();
+                    DoUndo();
                 }
             }
         }
+    }
+
+    private void DoUndo()
+    {
+        buttonGraying.GrayButtons(type, id);
+        List<(int, int, Color)> list = Paint.actionStack.Pop();
+        foreach ((int, int, Color) c in list)
+        {
+            Drawing.Texture.SetPixel(c.Item1, c.Item2, c.Item3);
+        }
+        Drawing.Texture.Apply();
     }
 }
